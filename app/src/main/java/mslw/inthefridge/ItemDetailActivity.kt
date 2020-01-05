104ace329b1c231e256403c2c0e3690b9be79dd4
Michał Szczepanik <m.szczepanik@nencki.edu.pl>
2020-01-05 18:40:23 +0100

    Add a template for item detail view and related activity
    Currently not sure how to connect it to the rest of the app.

---
+++ app/src/main/java/mslw/inthefridge/ItemDetailActivity.kt
@@ -0,0 +1,12 @@
+package mslw.inthefridge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ItemDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_detail)
+    }
+}
