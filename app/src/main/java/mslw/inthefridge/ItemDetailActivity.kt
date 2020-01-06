ed598878766e1b59d692d1c9e715b8ce4c549e92
Michał Szczepanik <m.szczepanik@nencki.edu.pl>
2020-01-06 17:23:33 +0100

    Allow deletion of items
    Deletion is handled through the ItemDetailActivity.

--- app/src/main/java/mslw/inthefridge/ItemDetailActivity.kt
+++ app/src/main/java/mslw/inthefridge/ItemDetailActivity.kt
@@ -1,8 +1,11 @@
 package mslw.inthefridge
 
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import android.widget.TextView
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 
@@ -27,6 +30,9 @@ class ItemDetailActivity : AppCompatActivity() {
         val foodExpView = findViewById<TextView>(R.id.detail_exp_textView)
         val foodShelfLifeView = findViewById<TextView>(R.id.detail_shelf_life_textView)
 
+        // Prepare handle for delete button
+        val deleteButton = findViewById<ImageButton>(R.id.detail_delete_button)
+
         // Create the observer which updates the UI
         val itemObserver = Observer<Food> {item ->
             foodNameView.text = item.name
@@ -37,7 +43,29 @@ class ItemDetailActivity : AppCompatActivity() {
         }
 
         // observe the LiveData containing selected food item
-        foodViewModel.selectById(idToDisplay).observe(this, itemObserver)
+        val selected = foodViewModel.selectById(idToDisplay)
+        selected.observe(this, itemObserver)
 
+        // Prepare an alert dialog which handles deletion confirmation
+        val dialogBuilder = AlertDialog.Builder(this)
+        dialogBuilder.setMessage(R.string.dialog_delete_question)
+        dialogBuilder.setPositiveButton(R.string.dialog_yes) { dialog, _ ->
+            dialog.dismiss()
+            deleteAndFinish(selected)
+        }
+        dialogBuilder.setNegativeButton(R.string.dialog_no) { dialog, _ ->  dialog.dismiss()}
+
+        // Hook up the delete button with the deletion dialog
+        val delAlertDialog = dialogBuilder.create()
+        deleteButton.setOnClickListener {
+            delAlertDialog.show()
+        }
+
+    }
+
+    private fun deleteAndFinish(selectedItem: LiveData<Food>){
+        selectedItem.removeObservers(this)  // don't wanna observe deleted item
+        foodViewModel.delete(selectedItem.value!!)
+        finish()
     }
 }
