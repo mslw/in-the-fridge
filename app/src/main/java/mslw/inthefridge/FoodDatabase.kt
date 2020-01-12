2d06c5ebf0704c813d323ae460d5a7dd5737c1df
Michał Szczepanik <m.szczepanik@nencki.edu.pl>
2020-01-12 21:09:53 +0100

    Remove database operations (incl. wipe) performed on launch
    They were useful for development, but not needed anymore.
    Left some of the code commented out.

--- app/src/main/java/mslw/inthefridge/FoodDatabase.kt
+++ app/src/main/java/mslw/inthefridge/FoodDatabase.kt
@@ -5,60 +5,13 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import androidx.sqlite.db.SupportSQLiteDatabase
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
-import java.time.LocalDateTime
-import java.util.*
 
 @Database(entities = arrayOf(Food::class), version = 1, exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class FoodDatabase: RoomDatabase() {
 
     abstract fun foodDao(): FoodDao
-
-    private class FoodDatabaseCallback(
-        private val scope: CoroutineScope
-    ) : RoomDatabase.Callback() {
-
-        // populate with some items - for development
-        override fun onOpen(db: SupportSQLiteDatabase) {
-            super.onOpen(db)
-            INSTANCE?.let { database ->
-                scope.launch {
-                    var foodDao = database.foodDao()
-
-                    // Delete all content
-                    foodDao.wipe()
-
-                    // Fake some dates
-                    val fakeOpen = Date(1578090519315)
-                    val fakeExp = Date(1578133719315)
-
-                    // Add an item
-                    var food = Food(
-                        name = "Juice",
-                        description = "Orange",
-                        openDate = fakeOpen,
-                        expiryDate = fakeExp,
-                        shelfLife = Pair(0, 12)
-                    )
-                    foodDao.insert(food)
-
-                    // Add another item
-                    food = Food(
-                        name = "Ketchup",
-                        description = "",
-                        openDate = fakeOpen,
-                        expiryDate = null,
-                        shelfLife = null
-                    )
-                    foodDao.insert(food)
-                }
-            }
-        }
-
-    }
 
     companion object {
         // singleton
@@ -75,7 +28,7 @@ abstract class FoodDatabase: RoomDatabase() {
                     context.applicationContext,
                     FoodDatabase::class.java,
                     "food_database"
-                ).addCallback(FoodDatabaseCallback(scope)).build()
+                ).build()
                 INSTANCE = instance
                 return instance
             }
